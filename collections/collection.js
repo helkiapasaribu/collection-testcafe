a4114c4f9f7e1e94226ffdacbaef7f34e0f4e739
helkiapasaribu <helkiapasaribu27@gmail.com>
2020-10-09 21:55:21 +0700

    Update Progress: Test Case Login Web Collection

--- collections/collection.js
+++ collections/collection.js
@@ -3,9 +3,14 @@ import { Selector } from 'testcafe'
 fixture `collection web automation testing using testcafe`
 .page(`https://mvp.uangteman.com/`)
 
-test ('click username field', async t => {
+//login
+test ('Login Collection Web', async t => {
 await t
 .setTestSpeed(0.3)
+.click(Selector('#username'))
+.typeText('#username','andrepangestu+col@uangteman.com')
+.typeText('#Password', 'UTEmployeeP@ssw0rd')
+.click('#root > div > div.css-1y8l2jl > form > div.css-5wh8sj > div:nth-child(1)')
 
-
+.wait(5000)
 })
